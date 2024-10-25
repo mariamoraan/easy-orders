fe761fa5f8789a9f02a976a787731095f5137ca2
Maria Moran <mariamoranluaces@gmail.com>
2024-10-25 22:49:37 +0200

    feat: add scroll into view behaviour

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,21 @@ import { AuthProvider } from './features/auth/delivery/context/auth.context';
 import '@/core/firebase';
 import { CompanyProvider } from './features/company/delivery/context/company.provider';
 import { OrdersProvider } from './features/orders/delivery/context/orders.provider';
+import { useEffect, useRef } from 'react';
 
 export const App = () => {
+  const scrollInto = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!scrollInto?.current) return;
+    scrollInto?.current.scrollIntoView();
+  }, [scrollInto]);
   return (
     <AuthProvider>
       <CompanyProvider>
         <OrdersProvider>
-          <Outlet />
+          <div ref={scrollInto}>
+            <Outlet />
+          </div>
         </OrdersProvider>
       </CompanyProvider>
     </AuthProvider>
