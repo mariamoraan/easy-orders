8bed11a448835c20b3cf00689f16472f8da90ec4
Maria Moran <mariamoranluaces@gmail.com>
2024-10-26 19:34:55 +0200

    fix: restablish pdf functionality

--- src/features/orders/delivery/components/printable-order/printable-order.component.tsx
+++ src/features/orders/delivery/components/printable-order/printable-order.component.tsx
@@ -3,10 +3,7 @@ import styles from './printable-order.module.css';
 import { Order } from '@/features/orders/domain/order';
 import { OrderDetail } from '../order-detail/order-detail.component';
 import { useTranslate } from '@/core/i18n/hooks/use-translate.hook';
-import generatePDF, { Margin, Options, Resolution } from 'react-to-pdf';
-import { useRef } from 'react';
-import { ActionButton } from '@/core/components/action-button/action-button.component';
-import { DownloadIcon } from '@/core/icons';
+import { useLayoutEffect, useRef } from 'react';
 const cn = bind(styles);
 
 interface Props {
@@ -14,40 +11,26 @@ interface Props {
   closePrintMode: () => void;
 }
 
-const options: Options = {
-  // default is `save`
-  // default is Resolution.MEDIUM = 3, which should be enough, higher values
-  // increases the image quality but also the size of the PDF, so be careful
-  // using values higher than 10 when having multiple pages generated, it
-  // might cause the page to crash or hang.
-  filename: 'advanced-example.pdf',
-  page: {
-    margin: Margin.NONE,
-  },
-  resolution: Resolution.HIGH,
-};
-
 export const PrintableOrder = (props: Props) => {
   const { order, closePrintMode } = props;
   const { t } = useTranslate();
   const targetRef = useRef<HTMLDivElement>(null);
   const printPdf = async () => {
-    await generatePDF(targetRef, options);
+    window.print();
     closePrintMode();
   };
 
+  useLayoutEffect(() => {
+    printPdf();
+  }, []);
+
   return (
-    <div>
-      <div className={cn('actions')}>
-        <ActionButton label={<DownloadIcon />} onClick={() => printPdf()} />
+    <div ref={targetRef} className={cn('wrapper')}>
+      <div className={cn('header')}>
+        <h2 className={cn('title')}>{t('order-detail.order')}</h2>
       </div>
-      <div ref={targetRef} className={cn('wrapper', 'print-mode')}>
-        <div className={cn('header')}>
-          <h2 className={cn('title')}>{t('order-detail.order')}</h2>
-        </div>
-        <div className={cn('content')}>
-          <OrderDetail order={order} />
-        </div>
+      <div className={cn('content')}>
+        <OrderDetail order={order} />
       </div>
     </div>
   );
