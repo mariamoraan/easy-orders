cf85bbfc2e16e67875ad3f0083a988d1dacec22e
Maria Moran <mariamoranluaces@gmail.com>
2024-10-27 19:28:10 +0100

    fix: remove scroll into view feature

--- src/app.tsx
+++ src/app.tsx
@@ -3,23 +3,15 @@ import { AuthProvider } from './features/auth/delivery/context/auth.context';
 import '@/core/firebase';
 import { CompanyProvider } from './features/company/delivery/context/company.provider';
 import { OrdersProvider } from './features/orders/delivery/context/orders.provider';
-import { useEffect, useRef } from 'react';
 import { NotificationsProvider } from './features/notifications/delivery/context/notifications.provider';
 
 export const App = () => {
-  const scrollInto = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (!scrollInto?.current) return;
-    scrollInto?.current.scrollIntoView();
-  }, [scrollInto]);
   return (
     <NotificationsProvider>
       <AuthProvider>
         <CompanyProvider>
           <OrdersProvider>
-            <div ref={scrollInto}>
-              <Outlet />
-            </div>
+            <Outlet />
           </OrdersProvider>
         </CompanyProvider>
       </AuthProvider>
