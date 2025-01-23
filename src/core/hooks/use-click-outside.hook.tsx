3d10c65a8970ac03abe4063e1bc25282a9303176
Maria Moran <mariamoranluaces@gmail.com>
2025-01-23 21:39:46 +0100

    fix: handle null ref in useClickOutside and initialize useRef in useInterval

--- src/core/hooks/use-click-outside.hook.tsx
+++ src/core/hooks/use-click-outside.hook.tsx
@@ -1,6 +1,6 @@
 import { RefObject, useEffect } from 'react';
 
-export const useClickOutside = (ref: RefObject<HTMLElement | undefined>, callback: () => void) => {
+export const useClickOutside = (ref: RefObject<HTMLElement | undefined | null>, callback: () => void) => {
   const handleClick = (event: MouseEvent) => {
     if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
       callback();
